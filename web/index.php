db848cfe16032b7911b2204ac99272e8e8240751
Ashwaq Shaik <114009436+ShadooowX@users.noreply.github.com>
2025-05-21 15:17:44 +0530

    index.php

--- web/index.php
+++ web/index.php
@@ -1,6 +1,8 @@
 <html>
   <body>
     <h1>Welcome to ESI Demo!</h1>
+    <h2>developed by Ashwaq Shaik</h2>
+    <p>for practice on ESI attack</p>
 
     <!-- Simulating ESI (edge side include) -->
     <esi:include src="/secret.php" />
